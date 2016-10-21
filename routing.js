a46b63af4cf9feb28b0c4ca90a49d6dcc9c82cde
AKASH REDDY EDA <akash.kf524@gmail.com>
2016-10-20 21:37:38 -0700

    update
    views from page 1 to page 5 are completed

--- routing.js
+++ routing.js
@@ -1,14 +1,35 @@
-var app = angular.module("TrinetPlatform", ['ngRoute']);
+var app = angular.module("TrinetPlatform", ['ngRoute', 'chart.js']);
 app.config(function ($routeProvider) {
 	$routeProvider
 		.when("/", {
-			templateUrl: "./Partials/home.html"
+			templateUrl: "./Partials/error.html"
 		})
-		.when("/scripting", {
-			templateUrl: "./Partials/scripting.html"
+		.when("/page1", {
+			controller: 'pieController',
+			templateUrl: "./Partials/page1.html"
 		})
-		.when("/vm", {
-			templateUrl: "./Partials/vm.html"
+		.when("/page2", {
+			templateUrl: "./Partials/page2.html"
+		})
+		.when("/page3", {
+			templateUrl: "./Partials/page3.html"
+		})
+		.when("/page4", {
+			controller: 'pieController2',
+			templateUrl: "./Partials/page4.html"
+		})
+		.when("/page5", {
+			templateUrl: "./Partials/page5.html"
+		})
+		.when("/page6", {
+			templateUrl: "./Partials/page3.html"
+		})
+		.when("/page7", {
+			templateUrl: "./Partials/page3.html"
+		})
+		.when("/page8", {
+			templateUrl: "./Partials/page3.html"
 		});
+
 
 });
